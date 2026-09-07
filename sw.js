3e9f030f8022b9bf83b695423d316fc6f028436e
Gursahib Singh <gursahib99888@gmail.com>
2026-09-07 13:50:05 -0700

    Publish

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  * and the old one is deleted on activate.
  */
 
-const BUILD = "20260907-203659";
+const BUILD = "20260907-204922";
 const CACHE = `gurbani-studio-${BUILD}`;
 
 /* Paths are relative so this works both at the site root and under a
